638ac573e205ab1a90e5ce046fa472d99c32131a
olgalmf <olgalmf2010@gmail.com>
2015-08-02 20:22:52 +0200

    DESPLIEGUE

---
+++ models/models.js
@@ -0,0 +1,45 @@
+var path =require('path');
+var pg=require('pg');
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+// Usar BBDD Sqlite 
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }     
+);
+// Importar la definicion de la tabla quiz en quiz.js
+var Quiz =sequelize.import(path.join(__dirname,'quiz'));
+exports.Quiz=Quiz;
+sequelize.sync().then(function(){
+	Quiz.count().then(function(count){
+		if(count===0){
+			Quiz.create({ pregunta:'Capital de Italia',
+						respuesta:'Roma'
+		});
+			Quiz.create({ pregunta:'Capital de Portugal',
+						respuesta:'Lisboa'
+
+		})
+
+			.then(function(){console.log('Base de datos inicializada')});
+		};
+	
+	
+});
+});
