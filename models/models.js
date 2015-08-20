2cf17e763b260f6cbdfbb3da175557b1c42eb53a
olgalmf <olgalmf2010@gmail.com>
2015-08-20 16:18:53 +0200

    Crear Comentarios

--- models/models.js
+++ models/models.js
@@ -1,6 +1,7 @@
-var path =require('path');
-var pg=require('pg');
+var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name  = (url[6]||null);
 var user     = (url[2]||null);
@@ -13,7 +14,8 @@ var storage  = process.env.DATABASE_STORAGE;
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
-// Usar BBDD Sqlite 
+
+// Usar BBDD SQLite o Postgres
 var sequelize = new Sequelize(DB_name, user, pwd, 
   { dialect:  protocol,
     protocol: protocol,
@@ -21,23 +23,33 @@ var sequelize = new Sequelize(DB_name, user, pwd,
     host:     host,
     storage:  storage,  // solo SQLite (.env)
     omitNull: true      // solo Postgres
-  }     
+  }      
 );
-// Importar la definicion de la tabla quiz en quiz.js
-var Quiz =sequelize.import(path.join(__dirname,'quiz'));
-exports.Quiz=Quiz;
-sequelize.sync().then(function(){
-	Quiz.count().then(function(count){
-		if(count===0){
-			Quiz.create({ pregunta:'Capital de Italia',
-						respuesta:'Roma',tema :'otro'
-		});
-			Quiz.create({ pregunta:'Capital de Portugal',
-						respuesta:'Lisboa',tema :'otro'
 
-		})	.then(function(){console.log('Base de datos inicializada')});
-		};
-	
-	
-});
+// Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
+exports.Comment = Comment; // exportamos Comentarios
+
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().then(function() {
+  // success(..) ejecuta el manejador una vez creada la tabla
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){ // la tabla se inicializa solo si está vacía
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
+      Quiz.bulkCreate( 
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma', tema:'otro'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa', tema:'otro'}
+        ]
+      ).then(function(){console.log('Base de datos inicializada')});
+    }
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
-
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz Show' , errors: [] });
-});
-
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-+router.get('/quizes/new', quizController.new);
-+router.post('/quizes/create', quizController.create);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
-router.put('/quizes/:quizId(\\d+)',  quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
+router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
 
-/* GET author page. */
-router.get('/author', function(req, res) {
-res.render('author.ejs');
-});
 module.exports = router;
