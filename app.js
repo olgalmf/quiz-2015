ea7c74d3fa10539f43eaa403737cf017299dd5ee
olgalmf <olgalmf2010@gmail.com>
2015-08-20 17:03:55 +0200

    Autenticación de Usuarios

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
+var methodOverride = require('method-override');
+var session = require('express-session');
 
-// importar enrutadores
 var routes = require('./routes/index');
 
-//creamos la aplicacion 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(partials());
 
 // uncomment after placing your favicon in /public
@@ -22,11 +23,25 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// Helpers dinamicos:
+app.use(function(req, res, next) {
 
+  // guardar path en session.redir para despues de login
+  if (!req.path.match(/\/login|\/logout/)) {
+    req.session.redir = req.path;
+  }
+
+  // Hacer visible req.session en las vistas
+  res.locals.session = req.session;
+  next();
+});
+
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,7 +59,8 @@ if (app.get('env') === 'development') {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err ,  errors: []
+            error: err,
+            errors: []
         });
     });
 }
@@ -55,7 +71,8 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {} , errors: []
+        error: {},
+        errors: []
     });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,33 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
-// Página de entrada (home page)
+/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz', errors: []});
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
 
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
+
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/author', function(req, res) { res.render('author', {errors: []} ) });
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
-router.get('/quizes/:quizId(\\d+)/comments/new',            commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',              commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
 
 module.exports = router;
