061ea3a21a65a46b6f52f13b816390c7463a4579
olgalmf <olgalmf2010@gmail.com>
2015-08-20 19:32:13 +0200

    Moderación de Cometarios

--- models/comment.js
+++ models/comment.js
@@ -10,5 +10,8 @@ module.exports = function(sequelize, DataTypes) {
       publicado: {
       	type: DataTypes.BOOLEAN,
       	defaultValue: false
-       }
-    }
+      }
+    }    
+  );
+}
+
--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,14 @@ var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz', errors: [] });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -22,14 +23,16 @@ router.get('/logout', sessionController.destroy); // destruir sesión
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/author', function(req, res) { res.render('author', {errors: []} ) });
-router.get('/quizes/new',                  sessionController.loginRequired, quizController.new);
+router.get('/quizes/new', 				   sessionController.loginRequired, quizController.new);
 router.post('/quizes/create',              sessionController.loginRequired, quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',   sessionController.loginRequired, quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
 
-router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+// Definición de rutas de comentarios
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+	                                    sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
